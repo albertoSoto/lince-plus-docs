aa156d618ade330193d7606e854088c89863fa16
Alberto Soto Fernandez <albert.soto@gmail.com>
2023-11-10 11:35:05 +0100

    initial doc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -134,7 +134,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} LINCE PLUS, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
